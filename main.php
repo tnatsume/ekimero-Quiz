62559bef62b96d0f28158126424ae29e87686d4b
tnatsume@vitalify.jp <tnatsume@vitalify.jp>
2019-08-07 14:45:06 +0900

    ボタンの高さを揃える修正

--- main.php
+++ main.php
@@ -2,9 +2,11 @@
     <div class="container">
         <p class="top">山手線駅メロクイズ</p>
         <form action="problem.php"method="POST">
-            <input type="text"class="name"name="name"placeholder="あなたの名前を教えてください"value=""autofocus required autocomplete="off">
-            <input type="submit"class="btn"value="問題に進む">
-            <a class="btn1"href="ranking.php"><span >ランキング</span></a>
+            <input type="text"class="name"name="name"placeholder="あなたの名前を教えてください"value=""autofocus required autocomplete="off"maxlength="20">
+            <div class="btn-field">
+                <input type="submit"class="btn"value="問題に進む">
+                <a class="btn1"href="ranking.php"><span >ランキング</span></a>
+            </div>
         </form>
     </div>
 </div>
